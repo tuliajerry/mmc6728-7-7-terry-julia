6c189445f2a6b021cb4d122c3ee0b0af50f939ac
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-10-15 13:58:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
-require('dotenv').config()
-const express = require('express')
-const exphbs = require('express-handlebars')
-// Import the sessions packages
-const apiRoutes = require('./routes/api-routes')
-const htmlRoutes = require('./routes/html-routes')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session'); 
+const MySQLStore = require('express-mysql-session')(session); 
+const apiRoutes = require('./routes/api-routes');
+const htmlRoutes = require('./routes/html-routes');
+const db = require('./db'); 
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Add code to use sessions with a MySQL Store
 
-app.engine('handlebars', exphbs.engine())
-app.set('view engine', 'handlebars')
+const sessionStore = new MySQLStore({}, db);
+app.use(session({
+  key: 'session_cookie',
+  secret: process.env.SESSION_SECRET,
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: false,
+  proxy: true,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24, 
+  },
+}));
 
-app.use(express.static('public'))
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
 
-app.use('/', htmlRoutes)
-app.use('/api', apiRoutes)
+app.use(express.static('public'));
 
-module.exports = app
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
+
+module.exports = app;
+
