73e23a252ff6758648df0c22212835d0f43121fe
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-10-15 16:20:26 -0400

    Update schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,6 +1,5 @@
 USE music_shop_db;
 
-
 CREATE TABLE inventory (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
@@ -10,23 +9,18 @@ CREATE TABLE inventory (
   quantity INT NOT NULL DEFAULT 0
 );
 
-
-CREATE TABLE users (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(100) NOT NULL UNIQUE,
-  password VARCHAR(200) NOT NULL
-);
-
+-- Create the users table with
+-- id that's primary key integer auto incrementing
+-- username that's a 100 char varchar and unique
+-- password that's a 200 char varchar
 
 CREATE TABLE cart (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   inventory_id INT NOT NULL,
-  user_id INT, 
+-- add a user_id column that's an int
   quantity INT NOT NULL DEFAULT 1,
   FOREIGN KEY (inventory_id)
     REFERENCES inventory (id)
-    ON DELETE CASCADE,
-  FOREIGN KEY (user_id)
-    REFERENCES users (id)  
     ON DELETE CASCADE
+-- add a foreign key constraint to user_id just like inventory_id
 );
