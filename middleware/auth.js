147efb9b5f4de2588e2ec05ee488a7af7c8ca536
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-10-15 16:53:01 -0400

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,9 @@
 function checkAuth(req, res, next) {
-	// Check req.session.loggedIn to see if the user is logged in
-	// If the user is logged in, call next()
-	// If the user is not logged in, redirect to /login
+  if (req.session.loggedIn) {
+    return next();
+  }
+  res.redirect('/login');
 }
 
-module.exports = checkAuth
+module.exports = checkAuth;
+
