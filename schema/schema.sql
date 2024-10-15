a484cbeed5a3dbdda65d8cab42109f0fcad6d197
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-10-15 13:57:55 -0400

    Update schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -9,18 +9,21 @@ CREATE TABLE inventory (
   quantity INT NOT NULL DEFAULT 0
 );
 
--- Create the users table with
--- id that's primary key integer auto incrementing
--- username that's a 100 char varchar and unique
--- password that's a 200 char varchar
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(100) NOT NULL UNIQUE,
+  password VARCHAR(200) NOT NULL
+);
 
 CREATE TABLE cart (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   inventory_id INT NOT NULL,
--- add a user_id column that's an int
+  user_id INT, 
   quantity INT NOT NULL DEFAULT 1,
   FOREIGN KEY (inventory_id)
     REFERENCES inventory (id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (user_id)
+    REFERENCES users (id)  
     ON DELETE CASCADE
--- add a foreign key constraint to user_id just like inventory_id
 );
